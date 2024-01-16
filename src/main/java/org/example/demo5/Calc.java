e71601aa0b89944c60fdaa4f5053839a0a7f5b95
Marta <=>
2024-01-16 15:45:39 +0100

    proba

--- src/main/java/org/example/demo5/Calc.java
+++ src/main/java/org/example/demo5/Calc.java
@@ -1,15 +1,18 @@
 package org.example.demo5;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 
-public class cpmCalc extends Controller {
+public class Calc extends Controller {
     protected ActionEvent event_act;
     protected ActionEvent event_goal;
     protected ActionEvent event_meals;
     protected String activity = getActivity(event_act);
     protected String goal = getGoal(event_goal);
     protected int meal_count = getMeals(event_meals);
+
+    public double cpm() {
+        
+    }
 
 //    proba
 
