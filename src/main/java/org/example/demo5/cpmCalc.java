ed5e6375f38a555d02a8c53fbc7c9948ef27664b
= <=>
2024-01-16 15:05:34 +0100

    proba

--- src/main/java/org/example/demo5/cpmCalc.java
+++ src/main/java/org/example/demo5/cpmCalc.java
@@ -1,8 +1,8 @@
-//package org.example.demo5;
-//
-//public class cpmCalc extends Controller{
-//    public static double cpm (double height, double weight, double act, String goal) {
-//        height =
-//
-//    }
-//}
+package org.example.demo5;
+
+public class cpmCalc extends Controller{
+    public static double cpm (double height, double weight, double act, String goal) {
+        height =
+
+    }
+}
