039e312d402d2a209020ae1d05588e170e286d4c
AnnCzar <268319@student.pwr.edu.pl>
2024-01-19 00:29:19 +0100

    open next window

--- src/main/java/org/example/demo5/MainApp.java
+++ src/main/java/org/example/demo5/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("view.fxml"));
-       
+
         Scene scene = new Scene(fxmlLoader.load(), 700, 700);
         stage.setTitle("Dzienne spożycie");
         stage.setScene(scene);
