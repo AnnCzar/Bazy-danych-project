d67028574f090e24f2c2625e0dafbaa13c36f720
Marta <=>
2024-01-19 09:36:02 +0100

    problem fixed

--- src/main/java/org/example/demo5/logController.java
+++ src/main/java/org/example/demo5/logController.java
@@ -1,9 +1,9 @@
 package org.example.demo5;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -12,59 +12,29 @@ import java.util.ResourceBundle;
 
 public class logController implements Initializable {
 
+    @FXML
+    protected Button next;
 
     @FXML
-    protected Button log;
+    protected Label us_name;
 
     @FXML
     protected TextField login;
 
     @FXML
-    protected Button next;
+    public void logOption(ActionEvent event) {
+        us_name.setVisible(true);
+        login.setVisible(true);
+        next.setVisible(true);
+    }
 
     @FXML
-    protected Button register;
-
-    @FXML
-    protected Label us_name;
+    public void regOption(ActionEvent event) {
+        next.setVisible(true);
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        log.setOnAction(event -> logOption());
-        register.setOnAction(event -> regOption());
-
-        //next.setOnAction(event -> buttonAction());
-    }
-    @FXML
-    public void logOption() {
-        us_name.setVisible(true);
-        login.setVisible(true);
-        if (login.getText() != null && !login.getText().trim().isEmpty()) {
-            next.setVisible(true);
-            log.setVisible(false);
-        } else {
-            next.setVisible(false);
-        }
-    }
-    @FXML
-    public void regOption() {
-        next.setVisible(true);
+        // Initialization code goes here
     }
 }
-
-//    public void acceptButton() {
-//        if (log.isSelected()) {
-//            us_name.setVisible(true);
-////            login.setVisible(true);
-//        } else if (login.getText() != null && !login.getText().trim().isEmpty()) {
-//            next.setVisible(true);
-//        } else {
-//            next.setVisible(false);
-//        }
-//////        buttonAction();
-//    }
-
-//    public void buttonAction() {
-//
-//    }
-
