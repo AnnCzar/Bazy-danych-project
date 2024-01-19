78902e528d3ac08fc6aeca86c1533217f223f7db
Marta <=>
2024-01-19 10:20:35 +0100

    another problem created

--- src/main/java/org/example/demo5/logController.java
+++ src/main/java/org/example/demo5/logController.java
@@ -2,11 +2,16 @@ package org.example.demo5;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -37,10 +42,37 @@ public class logController implements Initializable {
     public void regOption(ActionEvent event) {
         next.setVisible(true);
     }
+    @FXML
+    public void nextPressed(ActionEvent event) {
+        Stage stage = (Stage) log.getScene().getWindow();
+        if (event.getSource() == log) {
+            openMainApp(stage);
+        } else {
+            openMyApp(stage);
+        }
+    }
+
+    private void openMainApp(Stage stage) {
+        try {
+            MainApp mainApp = new MainApp();
+            mainApp.start(stage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openMyApp(Stage stage) {
+        try {
+            MyApp myApp = new MyApp();
+            myApp.start(stage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //next.setOnAction(this::buttonAction);
+        next.setOnAction(this::nextPressed);
         log.setOnAction(this :: logOption);
         register.setOnAction(this::regOption);
     }
